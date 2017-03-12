35e21fad352e89bf8659ceb2f64f08cc36daed75
Cyril <loki0119@gmail.com>
2017-03-12 04:26:47 -0700

    implement send sms message using lambda and api gateway

--- handler.js
+++ handler.js
@@ -1,15 +1,32 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+module.exports.SNS = (event, context, callback) => {
+  const headers = event.headers;
+  const phoneNumber = headers.phonenumber;
+  const restaurant = headers.restaurant;
+  const datetime = headers.datetime;
+  var AWS = require('aws-sdk');
+  AWS.config.region = 'us-east-1';
+  var sns = new AWS.SNS({
+      apiVersion: '2014-10-31',
+      accessKeyId: '',
+      secretAccessKey: ''
+  });
+  var params = {
+    Message: `You book ${restaurant} at ${datetime}`,
+    MessageStructure: 'string',
+    PhoneNumber: phoneNumber
   };
+  sns.publish(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+const response = {
+  statusCode: 200,
+  body: 'Send SMS Success'
+};
 
-  callback(null, response);
+callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
